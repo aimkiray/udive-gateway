a65a1e6c774fd3d45b4b10139418a5497fa20b08
aimkiray <root@meowwoo.com>
2020-04-27 16:51:22 +0800

    fix: one config

--- app/__init__.py
+++ app/__init__.py
@@ -9,40 +9,34 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 # from flask_wtf.csrf import CSRFProtect
-from app.serial import Serial
-from flask_socketio import SocketIO
-import configparser
+# from app.serial import Serial
+from app.opencv import OpenCV
+# from flask_socketio import SocketIO
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
-photos = UploadSet('photos', IMAGES)
-ser = Serial()
-socketio = SocketIO()
+# ser = Serial()
+cv = OpenCV()
+# socketio = SocketIO()
 # csrf = CSRFProtect()
-
-config = configparser.ConfigParser()
-config.read('config.ini')
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('app.config.' + config['DEFAULT']['Env'] + 'Config')
+    app.config.from_object('app.config.' + os.environ.get('Env') + 'Config')
 
     bootstrap.init_app(app)
     db.app = app
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
-    ser.init_app(app)
-    socketio.init_app(app)
+    # ser.init_app(app)
+    cv.init_app(app)
+    # socketio.init_app(app)
     login.login_view = 'surface.login'
-    # file
-    configure_uploads(app, photos)
-    patch_request_class(app, app.config['MAX_CONTENT_LENGTH'])  # set maximum file size, default is 16MB
     # csrf.init_app(app)
 
     try:
@@ -57,8 +51,8 @@ def create_app():
     from app import camera
     app.register_blueprint(camera.bp)
 
-    from app import sensor
-    app.register_blueprint(sensor.bp)
+    # from app import sensor
+    # app.register_blueprint(sensor.bp)
 
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
